c75ebeb6289a56eb5d24f040d747a8b404736499
AsukaKanwmatsu <asuka.kanematsu@en-gei.co.jp>
2023-04-22 00:22:47 +0900

    [PHP基礎]多次元配列の課題2の提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,13 @@
 <?php 
-$countries = [
-    "ブラジル" => 100,
-    "日本" => 150,
-    "スペイン" => 50000
-];
+$animals1 = ["いぬ" => 1, "ねこ" => 2, "うさぎ" => 6];
+$animals2 = ["ぞう" => 1, "きりん" => 1, "さい" => 4];
+$animalsList = ["小動物" => $animals1, "大動物" => $animals2];
 
-foreach ($countries as $name => $people) {
-  echo $name . ":" . $people . "人";
-  echo "\n";
+foreach ($animalsList as $animals) {
+  foreach ($animals as $animalName => $animalNum) {
+    if ($animalNum >= 2) {
+      $myOwnAnimalList[] = $animalName;
+    }
+  }
 }
+echo implode("\n", $myOwnAnimalList);
