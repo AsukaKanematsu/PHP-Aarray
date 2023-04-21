4403b1601f9f63a7ea8290bf4d23bb8af7ef6ed9
AsukaKanwmatsu <asuka.kanematsu@en-gei.co.jp>
2023-04-21 23:27:36 +0900

    [PHP基礎]連想配列、多次元配列の課題1,2の提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,11 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$countries = [
+    "ブラジル" => 100,
+    "日本" => 150,
+    "スペイン" => 50000
+];
+
+foreach ($countries as $name => $people) {
+  echo $name . ":" . $people . "人";
+  echo "\n";
+}
